ad0d90c41498780f04fa07ff7f77fe3c3b071289
kira07sj <kirubeldagne1@gmail.com>
2024-08-11 14:17:29 -0700

    stats section completed, business section init

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,7 +3,19 @@ import styles from "../style"
 
 const Stats = () => {
   return (
-    <section>
+    <section className={`${styles.flexCenter} flex-row
+     flex-wrap sm:mb-20 mb-6 gap-10 max-sm:grid`}>
+      {stats.map((stat)=>(
+        <div key= {stat.id} className={`text-white ${styles.flexCenter}
+         `}>
+          <h4 className="font-semibold font-poppins
+           xs:text-[40px] text-[30px] xs:leading[53px]
+            leading-[43px]">{stat.value}</h4>
+          <p className="font font-poppins
+           xs:text-[20px] text-[15px] xs:leading[26px]
+           leading-[21px] text-gradient uppercase ml-3">{stat.title}</p>
+        </div>
+      ))}
       
     </section>
   )
